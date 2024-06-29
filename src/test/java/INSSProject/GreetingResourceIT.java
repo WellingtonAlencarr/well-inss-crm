2f56af9df183789a5e5400192bbfa000b4df9095
Wellington Alencarr <wellington_silva41@hotmail.com>
2024-06-29 00:51:17 -0300

    first commit

---
+++ src/test/java/INSSProject/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package INSSProject;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
